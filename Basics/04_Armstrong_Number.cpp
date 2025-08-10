c7a818d614ac8cf489855dda248e40130a5bade2
Yaswanth Reddy <yaswanth.glory@gmail.com>
2025-08-10 20:49:58 +0530

    04_Armstrong_Number Completed

---
+++ Basics/04_Armstrong_Number.cpp
@@ -0,0 +1,36 @@
+//Armstrong Number Program using Cpp
+#include <bits/stdc++.h>
+using namespace std;
+int digitCount(int n){
+    int count = 0;
+    while(n!=0){
+        n/=10;      //for each iteration last digit will be removed
+        count++;   //for each removed digit, count will be incremented
+    }
+    return count;
+}
+bool isArmstrong(int n){
+    int originalNumber = n;     //duplicating original number
+    int totalDigits = digitCount(n); //returns the number of digits
+    int armstrongSum = 0;
+    while(n!=0){
+        int lastDigit = n%10;
+        armstrongSum = armstrongSum+int(pow(float(lastDigit),float(totalDigits)));
+        //pow only allows decimal type, thats why converting int to float 
+        n /=10;
+    }
+    return originalNumber == armstrongSum;
+
+}
+int main(){
+    int num;
+    cout<<"Enter a number to check Armstrong or not: ";
+    cin>>num;
+    if(isArmstrong(num)){
+        cout<<num<<" is a armstrong number"<<endl;
+    }
+    else{
+        cout<<num<<" is not a armstrong number"<<endl;
+    }
+    return 0;
+}
