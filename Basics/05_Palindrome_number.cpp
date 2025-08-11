c73713ad1660b1e9769118bc7613c3bdb8c52c33
Yaswanth Reddy <yaswanth.glory@gmail.com>
2025-08-11 12:46:47 +0530

    05_Palindrom_Number completed

---
+++ Basics/05_Palindrome_number.cpp
@@ -0,0 +1,32 @@
+/*
+Program: Palindrom Number
+Language: Cpp
+Author: Yash
+*/
+#include <bits/stdc++.h>
+using namespace std;
+int reverseNumber(int n){
+    int rev = 0;
+    while(n!=0){
+        int lastDigit = n%10;   //lastDigit is extracted
+        rev = (rev*10)+lastDigit; 
+        n = n/10;     //now, last digit is removing
+    }
+    return rev;
+}
+int main(){
+    int num;
+    cout<<"Enter an integer: ";
+    cin>>num;
+    if(num<0){
+        num = abs(num);
+    }
+    int reversedNumber = reverseNumber(num);
+    if(reversedNumber == num) {
+        cout<< "The number "<<num<<" is palindrome."<<endl;
+    }
+    else {
+        cout<<"The number "<<num<<" is not palindrome."<<endl;
+    }
+    return 0;
+}
